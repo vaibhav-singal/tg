3374e435d43c2f4143cd909764065e5c885a91fb
vaibhav-singal <vaibhavsingal903@gmail.com>
2020-12-21 20:33:29 +0530

    Made changes in Project 36

--- Food.js
+++ Food.js
@@ -1,48 +1,37 @@
-class Food{
-    constructor(){
-       this.image = loadImage("Milk.png");
-       var foodStock;
-       var lastFed;
+class Food {
+    constructor() {
+        this.image = loadImage("Milk.png");
+        this.foodStock = 0; this.lastFed;
 
-
-    }
-
-    getFoodStock() {
-        var foodCountRef = database.ref('Food');
-        foodCountRef.on("value",function(data){
-           foodStock  = data.val();
-        })
-    }
- 
-    updateFoodStock(count){
-     database.ref('/').update({
-         foodCount: count
-     })
-   }
- 
- 
-   
-   
-
-    display(){
-
-        var x=80,y=100;
+    } getFoodStock() {
         
-        imageMode(CENTER);
-        image(this.image,720,220,70,70);
+        return
 
-        if(this.foodStock!=0){
-            for(var i=0;i<this.foodStock;i++){
-                if(i%10==0){
-                    x=x+50;
-                    y=130
+         this.foodStock;
+    }
+    updateFoodStock(foodStock) {
+        this.foodStock = foodStock;
+    } deductFood() {
+        if (this.foodStock > 0) {
+            this.foodStock = this.foodStock - 1;
+        }
+
+    }
+
+    getFedTime(lastFed) {
+        this.lastFed = lastFed;
+    }
+    display() {
+        var x = 80, y = 100; imageMode(CENTER);
+        image(this.image, 720, 220, 70, 70);
+        if (this.foodStock != 0) {
+            for (var i = 0; i < this.foodStock; i++) {
+                if (i % 10 == 0) {
+                    x = x + 50; y = 130
                 }
-                this.image(this.image,x,y,50,50);
-                x=x+30;
+                image(this.image, x, y, 50, 50);
+                x = x + 30;
             }
         }
-     
-
     }
-    
 }
